97325c8ed4771538e93a831ffcd920f713e03aec
Karsen H <karsenhansen@gmail.com>
2024-12-13 22:54:08 -0800

    started building resume editor

---
+++ src/lib/validation.ts
@@ -0,0 +1,30 @@
+// Form schemas
+import { z } from 'zod'
+
+export const optionalString = z.string().trim().optional().or(z.literal(''))
+
+export const generalInfoSchema = z.object({
+    title: optionalString,
+    description: optionalString,
+})
+
+export type GeneralInfoTypes = z.infer<typeof generalInfoSchema>
+
+export const personalInfoSchema = z.object({
+    photo: z.custom<File | undefined>(
+        ).refine(
+            file => !file || (file instanceof File && file.type.startsWith('image/')), "Must be an image file."
+        ).refine(
+         file => !file || file.size <= 1024 * 1024 * 4, "File size must be less than 4MB."   
+        ),
+    firstName: optionalString,
+    lastName: optionalString,
+    jobTitle: optionalString,
+    city: optionalString,
+    country: optionalString,
+    phone: optionalString,
+    email: optionalString,
+})
+
+export type PersonalInfoTypes = z.infer<typeof personalInfoSchema>
+
