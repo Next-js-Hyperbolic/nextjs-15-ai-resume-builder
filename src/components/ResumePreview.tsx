00e085444780828072594d248a39d7d24248657e
Karsen H <karsenhansen@gmail.com>
2024-12-15 14:40:39 -0800

    added dynamic zoom for preview content to stay sized

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,5 +1,7 @@
+import useDimensions from "@/hooks/useDimensions";
 import { cn } from "@/lib/utils";
 import { ResumeTypes } from "@/lib/validation";
+import { useRef } from "react";
 
 interface ResumePreviewProps {
   resumeData: ResumeTypes;
@@ -10,16 +12,26 @@ export default function ResumePreview({
   resumeData,
   className,
 }: ResumePreviewProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { width } = useDimensions(containerRef);
+
   return (
     <div
       className={cn(
         "aspect-[210/297] h-fit w-full bg-white text-black",
         className,
       )}
+      ref={containerRef}
     >
-      <h1 className="p-6 text-3xl font-bold">
-        This text should change with the size of the container div
-      </h1>
+      <div
+        className={cn("space-y-6 p-6", !width && "invisible")}
+        style={{ zoom: (1 / 794) * width }}
+      >
+        <h1 className="p-6 text-3xl font-bold">
+          This text should change with the size of the container div
+        </h1>
+      </div>
     </div>
   );
 }
