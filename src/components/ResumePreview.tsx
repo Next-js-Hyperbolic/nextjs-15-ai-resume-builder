f8a0d050177e620cacf1fd3e1f88c781058a522e
Karsen H <karsenhansen@gmail.com>
2024-12-21 15:58:52 -0800

    added ability to print resume with react to print

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -9,11 +9,13 @@ import { BorderStyles } from "@/app/(main)/editor/BorderStyleButton";
 
 interface ResumePreviewProps {
   resumeData: ResumeTypes;
+  contentRef?: React.Ref<HTMLDivElement>;
   className?: string;
 }
 
 export default function ResumePreview({
   resumeData,
+  contentRef,
   className,
 }: ResumePreviewProps) {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -31,6 +33,8 @@ export default function ResumePreview({
       <div
         className={cn("space-y-6 p-6", !width && "invisible")}
         style={{ zoom: (1 / 794) * width }}
+        ref={contentRef}
+        id="resumePreviewContent" // Added id to target in CSS for print styling.
       >
         <PersonalInfoHeader resumeData={resumeData} />
         <SummarySection resumeData={resumeData} />
