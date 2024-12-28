38381020ca8f85ece69e9069cd8679e8dc904efe
Karsen H <karsenhansen@gmail.com>
2024-12-28 15:37:53 -0800

    added premium modal to app

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,6 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-screen flex-col">
       <Navbar />
       {children}
+      <PremiumModal />
     </div>
   );
 }
